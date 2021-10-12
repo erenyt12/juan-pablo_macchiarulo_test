f5b89c13b0061315da21ff57408be8dc8b7e935d
erenyt12 <erenyt12@gmail.com>
2021-10-11 22:50:13 -0300

    correccion punto 3

--- Test/punto_3.py
+++ Test/punto_3.py
@@ -1,5 +1,5 @@
 # Inicio de mi programa
-mi_variable = 4
+mi_variable = 7
 
 if mi_variable == 0:
     print("Ha ingresado a la primera validación.")
@@ -15,3 +15,19 @@ else:
 
 
 # Fin del código
+
+
+#Solucion al problema.
+# mi_variable = 7
+
+# if mi_variable == 0:
+#     print("Ha ingresado a la primera validación.")
+
+# elif (mi_variable > 3) and (mi_variable< 6):
+#     print("Ha ingresado a la segunda validación.")
+
+# elif mi_variable > 6:
+#     print("Ha ingresado a la tercer validación.")
+
+# else: 
+#     print("No hay coincidencias.")
